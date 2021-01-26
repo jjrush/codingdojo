c9f224d0e1aac99e4a0e86fd74811f249d18944f
jjrush <jjrush-github@proton.me>
2021-01-25 19:18:41 -0800

    adding loops_algo assignment

---
+++ web_fundamentals/09_loops_algo/loops_algo.js
@@ -0,0 +1,13 @@
+for(var i = 1; i < 20; i+=2)
+{
+    console.log("Num: " + i);
+}
+
+var sum = 0;
+for(var i = 1; i < 5; i++)
+{
+    sum += i;
+    console.log("Ind: " + i);
+    console.log("Sum: " + sum);
+}
+
