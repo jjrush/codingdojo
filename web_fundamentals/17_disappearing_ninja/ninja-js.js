307bf2e75ff9d8c076e39a1a0204c48a00c47e4d
jjrush <jjrush-github@proton.me>
2021-02-02 20:40:57 -0800

    adding background color to button

--- web_fundamentals/17_disappearing_ninja/ninja-js.js
+++ web_fundamentals/17_disappearing_ninja/ninja-js.js
@@ -1,35 +1,35 @@
 // load the following when the DOM is ready
 $(document).ready(function() {
     // toggle between hiding or showing my id
-    $('#ninja-button1').click( function(selector) {
+    $('#ninja-button1').click( function() {
         $(".ninja1").fadeOut()
     });
 
-    $('#ninja-button2').click( function(selector) {
+    $('#ninja-button2').click( function() {
         $(".ninja2").fadeOut()
     });
 
-    $('#ninja-button3').click( function(selector) {
+    $('#ninja-button3').click( function() {
         $(".ninja3").fadeOut()
     });
 
-    $('#ninja-button4').click( function(selector) {
+    $('#ninja-button4').click( function() {
         $(".ninja4").fadeOut()
     });
 
-    $('#ninja-button5').click( function(selector) {
+    $('#ninja-button5').click( function() {
         $(".ninja5").fadeOut()
     });
 
-    $('#ninja-button6').click( function(selector) {
+    $('#ninja-button6').click( function() {
         $(".ninja6").fadeOut()
     });
 
-    $('#ninja-button7').click( function(selector) {
+    $('#ninja-button7').click( function() {
         $(".ninja7").fadeOut()
     });
 
-    $('#ninja-button8').click( function(selector) {
+    $('#ninja-button8').click( function() {
         $(".ninja8").fadeOut()
     });
 
