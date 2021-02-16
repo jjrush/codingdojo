a01cd380de86df5679664f58d95c4c14ef02982d
Jason Rush <jrush@nevada.unr.edu>
2021-02-15 21:47:29 -0800

    adding first jsx app

---
+++ mern/01_react/11_first_jsx/first-jsx/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <h1>Hello Dojo!</h1>
+        <ul>
+          <li>Learn react</li>
+          <li>Climb Mt Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </body>
+    </div>
+  );
+}
+
+export default App;
