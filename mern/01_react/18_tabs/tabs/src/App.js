01190b6639c6b83f1d6158b9157b1d1aa8c68c38
jjrush <jjrush-github@proton.me>
2021-03-03 22:27:04 -0800

    updating tabs assignment, adding coin flip

--- mern/01_react/18_tabs/tabs/src/App.js
+++ mern/01_react/18_tabs/tabs/src/App.js
@@ -4,7 +4,6 @@ import Tabs from './components/Tabs';
 import Output from './components/Output';
 
 function App() {
-    // create an array of items that will have a label and a content property.
     const tabsArray = [
     { label: "Tab 1", content: "Tab 1 content is showing here" },
     { label: "Tab 2", content: "Tab 2 content is showing here" },
@@ -12,17 +11,12 @@ function App() {
     { label: "Tab 4", content: "Tab 4 content is showing here" },
     ];
 
-    // create state that can hold our array of items
-    //    technically we will never use setAllTabs since it is set when we initialize it
-    //    and we do not need to ever change it
     const [ allTabs, setAllTabs ] = useState(tabsArray);
 
     const [ currentTabIndex, setCurrentTabIndex ] = useState(0);
 
     return (
         <div className="App">
-        {/* When the line gets really long, you can break it out over multiple lines */}
-        {/*   to make it more readable */}
             <Tabs 
                 allTabs={ allTabs } 
                 currentTabIndex={ currentTabIndex }
