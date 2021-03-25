622b48193cb8118bbc13820f70e129267fa5b4fd
Jason Rush <jrush@nevada.unr.edu>
2021-03-24 21:38:42 -0700

    product manager 1 assignment

---
+++ mern/04_full_stack/product_manager_1/client/src/views/Main.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import ProductForm from '../components/ProductForm';
+import '../css/main.css'
+import '../css/productform.css'
+
+const Main = () => {
+    return (
+        <div className="App">
+            <h1>Product Manager</h1>
+            <ProductForm className="productForm"/>
+        </div>
+    )
+}
+export default Main;
