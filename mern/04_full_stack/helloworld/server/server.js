1cc72bfc400e054aa75dc1bfaf84923deb35ea4a
Jason Rush <jrush@nevada.unr.edu>
2021-03-24 20:45:31 -0700

    adding the database piece and react

--- mern/04_full_stack/helloworld/server/server.js
+++ mern/04_full_stack/helloworld/server/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const cors = require('cors')    /* This is new */
-
+const cors = require('cors');
 const app = express();
-app.use(cors())                 /* This is new */
-
+require('./config/mongoose.config');               /* This is new */
+app.use(cors());
+app.use(express.json());                           /* This is new */
+app.use(express.urlencoded({ extended: true }));   /* This is new */
 require('./routes/person.routes')(app);
-
 app.listen(8000, () => {
     console.log("Listening at Port 8000")
 })
