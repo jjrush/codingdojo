22171c2b900e762f1a05d5c3c5f7ce385ea2ce13
jjrush <jjrush-github@proton.me>
2021-03-24 21:55:59 -0700

    adding fetching from database to front end

--- mern/04_full_stack/helloworld/client/src/App.js
+++ mern/04_full_stack/helloworld/client/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Router } from '@reach/router';   /* this is new */
 import Main from './views/Main';
+import Detail from './views/Detail';
 function App() {
     return (
         <div className="App">
-            <Main />
+            <Router>                            
+                <Main path="/people/" default/>
+                <Detail path="/people/:id" />
+            </Router>                           
         </div>
     );
 }
--- mern/04_full_stack/helloworld/server/routes/person.routes.js
+++ mern/04_full_stack/helloworld/server/routes/person.routes.js
@@ -1,5 +1,7 @@
 const PersonController = require('../controllers/person.controller');
 module.exports = function(app){
     app.get('/api', PersonController.index);
+    app.get('/api/people', PersonController.getAllPeople);
+    app.get('/api/people/:id', PersonController.getPerson);
     app.post('/api/people', PersonController.createPerson);     /* This is new */
 }
