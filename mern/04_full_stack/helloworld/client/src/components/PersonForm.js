c3aa5bfdd6cc4d6fb0578cd2984c155a691742e6
jjrush <jjrush-github@proton.me>
2021-03-28 17:53:29 -0700

    refactoring

--- mern/04_full_stack/helloworld/client/src/components/PersonForm.js
+++ mern/04_full_stack/helloworld/client/src/components/PersonForm.js
@@ -1,33 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-const PersonForm = () => {
-    //keep track of what is being typed via useState hook
-    const [firstName, setFirstName] = useState(""); 
-    const [lastName, setLastName] = useState("");
-    //handler when the form is submitted
+const PersonForm = (props) => {
+    const { initialFirstName, initialLastName, onSubmitProp } = props;
+    const [firstName, setFirstName] = useState(initialFirstName);
+    const [lastName, setLastName] = useState(initialLastName);
     const onSubmitHandler = e => {
-        //prevent default behavior of the submit
         e.preventDefault();
-        //make a post request to create a new person
-        axios.post('http://localhost:8000/api/people', {
-            firstName,    // this is shortcut syntax for firstName: firstName,
-            lastName      // this is shortcut syntax for lastName: lastName
-        })
-            .then(res=>console.log(res))
-            .catch(err=>console.log(err))
+        onSubmitProp({firstName, lastName});
     }
-    //onChange to update firstName and lastName
+        
     return (
         <form onSubmit={onSubmitHandler}>
             <p>
-                <label>First Name</label><br/>
-                <input type="text" onChange = {(e)=>setFirstName(e.target.value)}/>
+                <label>First Name</label><br />
+                <input 
+                    type="text" 
+                    name="firstName" 
+                    value={firstName} 
+                    onChange={(e) => setFirstName(e.target.value)}
+                />
             </p>
             <p>
-                <label>Last Name</label><br/>
-                <input type="text" onChange = {(e)=>setLastName(e.target.value)}/>
+                <label>Last Name</label><br />
+                <input 
+                    type="text" 
+                    name="lastName" 
+                    value={lastName} 
+                    onChange={(e) => setLastName(e.target.value)}
+                />
             </p>
-            <input type="submit"/>
+            <input type="submit" />
         </form>
     )
 }
--- mern/04_full_stack/helloworld/client/src/views/Main.js
+++ mern/04_full_stack/helloworld/client/src/views/Main.js
@@ -5,21 +5,25 @@ import PersonList from '../components/PersonList';
 const Main = () => {
     const [people, setPeople] = useState([]);
     const [loaded, setLoaded] = useState(false);
-    useEffect(()=>{
+    useEffect(() => {
         axios.get('http://localhost:8000/api/people')
-            .then(res=>{
-                setPeople(res.data);
+            .then(res =>{ 
+                setPeople(res.data)
                 setLoaded(true);
             });
-    },[]);
-    
+    }, [])
     const removeFromDom = personId => {
         setPeople(people.filter(person => person._id != personId));
     }
-    
+    const createPerson = person => {
+        axios.post('http://localhost:8000/api/people', person)
+            .then(res=>{
+                setPeople([...people, res.data]);
+            })
+    }
     return (
         <div>
-            <PersonForm/>
+            <PersonForm onSubmitProp={createPerson} initialFirstName="" initialLastName=""/>
             <hr/>
             {loaded && <PersonList people={people} removeFromDom={removeFromDom}/>}
         </div>
