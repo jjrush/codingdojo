1df284e8c93470a2e3f8bdc426bf3da9efaf9c3d
jjrush <jjrush-github@proton.me>
2021-03-30 20:55:37 -0700

    adding socket assignment

---
+++ mern/04_full_stack/sockets/server/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const socket = require('socket.io');
+const port = 8000;
+app.use(cors());
+
+const server = app.listen(port, () => {
+    console.log(`Listening on port: ${port}`)
+});
+
+// to initialize the socket, we need to invoke a new instance
+//     of socket.io and pass it our express server instance
+// We must also include a configuration settings object to prevent CORS errors
+const io = socket(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'],
+        allowedHeaders: ['*'],
+        credentials: true,
+    }
+});
+
+io.on("connection", socket => {
+    // NOTE: Each client that connects get their own socket id!
+    // if this is logged in our node terminal, that means we a new client
+    //     has successfully completed the handshake!
+    console.log('socket id: ' + socket.id);
+    
+    // We add our additional event listeners right inside this function
+    // NOTE: "connection" is a BUILT IN events listener
+
+
+    socket.on("button_clicked", data => {
+        // send a message with "data" to ALL clients EXCEPT for the one that emitted the
+    	//     "event_from_client" event
+        console.log(data)
+        //socket.broadcast.emit("event_to_all_other_clients", data);
+    });
+});
+
